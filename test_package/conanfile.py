8f7fcf53ae4786acdc56a7b180907fc819a317da
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2020-04-21 08:18:01 +0300

    Test package build with Ninja

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,24 +1,22 @@
-# Tests for tinyxml2 Conan package
-# Dmitriy Vetutnev, Odant 2019
+# Test Conan package
+# Dmitriy Vetutnev, Odant 2019 - 2020
 
 
-from conans import ConanFile, CMake
+from conans import ConanFile, CMake, tools
 
 
 class PackageTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
-
-    def build_requiments(self):
-        if self.options["tinyxml2"].ninja:
-            self.build_requires("ninja_installer/1.9.0@bincrafters/stable")
+    requires = "ninja_installer/1.9.0@bincrafters/stable"
 
     def imports(self):
         self.copy("*.pdb", dst="bin", src="bin")
+        self.copy("*.dll", dst="bin", src="bin")
+        self.copy("*.so*", dst="bin", src="lib")
 
     def build(self):
-        gen = "Ninja" if self.options["tinyxml2"].ninja else None
-        cmake = CMake(self, generator=gen, msbuild_verbosity='normal')
+        cmake = CMake(self, generator="Ninja", msbuild_verbosity='normal')
         cmake.verbose = True
         cmake.configure()
         cmake.build()
@@ -29,3 +27,4 @@ class PackageTestConan(ConanFile):
             self.run("ctest --verbose --build-config %s" % self.settings.build_type)
         else:
             self.run("ctest --verbose")
+
